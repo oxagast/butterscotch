50df2c8cd3d2be7931526667394b0fb9eda4b118
Marshall Whittaker <marshall@oxasploits.com>
2025-03-24 18:05:30 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # btrfs-backups
 Tools that ease the use of BTRFS
+
+## Usage
+
+```
+BTRFS Snaptime, (c) 2025 oxasploits, llc.
+Designed by: oxagast / Marshall Whittaker.
+
+Usage:
+   ./btrfs-snaptime.sh -p /:/home -c -w
+   ./btrfs-snaptime.sh -p /home -r -d 5
+
+ -h       This help message.
+ -p       Partitions to snapshot, seperated by colons. Mandatory.        Default:          none
+ -d       Max number of snapshots to leave in trail, total.              Default:             6
+ -r       If there is a previous snapshot taken on the same              Default:           off
+          day, should we remove it and resnap?
+ -c       Immediately commit deletions.                                  Default:           off
+ -w       Mark read-only                                                 Default:           off
+ -L       Snapshot locations (relative to btrfs mountpoint)              Default:  /.snapshots/
+```
+
+## Author
+
+Marshall Whittaker (oxagast)
---
+++ btrfs-snaptime.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+# oxagast
+#
+# these are the user definable vars defaults
+LEAVEN=6 # the number of snapshots trailing the one you created that aren't deleted
+REDO=0
+CR=0
+RO=0
+SSDIR="/.snapshots/" # this is the dir under the btrfs mountpoint we should store backups in
+#
+function help {
+      echo "BTRFS Snaptime, (c) 2025 oxasploits, llc."
+      echo "Designed by: oxagast / Marshall Whittaker."
+      echo
+      echo "Usage:"
+      echo "   $0 -p /:/home -c -w"
+      echo "   $0 -p /home -r -d 5"
+      echo
+      echo " -h       This help message."
+      echo " -p       Partitions to snapshot, seperated by colons. Mandatory.        Default:          none"
+      echo " -d       Max number of snapshots to leave in trail, total.              Default:             6"
+      echo " -r       If there is a previous snapshot taken on the same              Default:           off"
+      echo "          day, should we remove it and resnap?"
+      echo " -c       Immediately commit deletions.                                  Default:           off"
+      echo " -w       Mark read-only                                                 Default:           off"
+      echo " -L       Snapshot locations (relative to btrfs mountpoint)              Default:  /.snapshots/"
+      echo
+}
+if [[ $# -eq 0 ]]; then
+  help
+  echo "You the -p argument is required."
+  exit 1
+fi
+# generates date
+D=$(date +snap-%d-%m-%Y)
+while getopts ":hp:d:rwcl:" OPTS; do
+  case ${OPTS} in
+    h) # display Help
+      help
+      exit 1
+      ;;
+    n) # how many to leave total
+      LEAVEN=${OPTARG} ;;
+    L) # location
+      SSDIR=${OPTARG} ;;
+    r) # if we need to remove todays snapshot first (using this too much is hard on your disk!)
+      REDO=1 ;;
+    p) # the partitions string
+      PTNSTR=${OPTARG} ;;
+    c) # commit removes
+      CR=1 ;;
+    w) # read-only fs
+      RO=1 ;;
+    \?) # invalid opt
+      help
+      echo "Error: Invalid option"
+      exit 1
+      ;;
+  esac
+done
+echo "Butter Snaptime, (c) 2024 oxasploits, llc."
+echo "Designed by oxagast."
+echo
+
+
+if [[ $(id -u) != 0 ]]; then
+  echo "This program needs to be run as root!"
+  echo "Use -h for help."
+  exit 1
+fi
+if [[ ${PTNSTR} == "" ]]; then
+  echo "You need to specify at least one partition to snapshot.  Multiple snapshots are split by a colon (:)."
+  echo "Use -h for help."
+  exit 1
+fi
+IFS=':'
+read -a PTN <<< "${PTNSTR}"
+if [[ ${LEAVEN} < 1 ]]; then
+  echo "You should leave at least one (1) backup snapshot!"
+  echo "Use -h for help."
+  exit 1
+fi
+IFS=' '
+for BTRFSP in "${PTN[@]}"; do
+  if [ ! -d "${BTRFSP}${SSDIR}" ]; then
+    mkdir -p "${BTRFSP}${SSDIR}" && echo "Directory created..." || echo "Directory creation failed"
+  fi
+  # removes any snapshots older than x days while leaving at least y snapshots
+  if [[ ${CR} == 0 ]]; then
+    find "/${BTRFSP}${SSDIR}" -maxdepth 0 -exec ls -1ctr {} \; | head -n -${LEAVEN} | xargs -I {} -d '\n' btrfs subvolume delete "/${BTRFSP}${SSDIR}"{}
+  else
+    find "/${BTRFSP}${SSDIR}" -maxdepth 0 -exec ls -1ctr {} \; | head -n -${LEAVEN} | xargs -I {} -d '\n' btrfs subvolume delete -c "/${BTRFSP}${SSDIR}"{}
+  fi
+  # check if redo is set and remove today's snap if it is
+  if [[ ${REDO} == 1 ]]; then
+    echo "Checking if there is a snapshot from today that needs removing before we can continue..."
+    if [ -d "${BTRFSP}${SSDIR}${D}" ]; then
+      if [[ ${CR} == 0 ]]; then
+        btrfs subvolume delete "${BTRFSP}${SSDIR}${D}" && echo "Removed todays snapshot..." # remove todays snapshot
+      else
+        btrfs subvolume delete -c "${BTRFSP}${SSDIR}${D}" && echo "Removed todays snapshot..." # remove todays snapshot
+      fi
+    else
+      echo "There was no snapshot from today to remove..."
+    fi
+  fi
+  # unless the snapshot already exists
+  if [ ! -d "${BTRFSP}${SSDIR}${D}" ]; then
+    # generate snapshot
+    btrfs subvolume snapshot ${BTRFSP} "${BTRFSP}${SSDIR}${D}" && echo "Subvolume snapshot taken: ${BTRFSP}."
+    # fix permissions on it
+    chmod a+rx,g+rx,u=rwx,o-w "${BTRFSP}${SSDIR}${D}" && echo "Permission earliest level fixed (a+rx,g+rx,u=rwx,o-w)."
+    if [ $RO == 1 ]; then
+      btrfs property set "${BTRFSP}${SSDIR}${D}" ro true
+      echo "Snapshot set as read-only."
+    fi
+  else
+    help
+    echo "Already snapped today. Hint: Try -r to override."
+    exit 1
+  fi
+  # loop back around for next
+done
+# now we're done
